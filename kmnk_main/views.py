8a516cf10b71be3ec0bc34f605f415bf731b3ff5
Mikhail Ivanov <kinoafisharu@gmail.com>
2018-07-10 15:14:34 +0300

    добавил фото в статику

--- kmnk_main/views.py
+++ kmnk_main/views.py
@@ -1,12 +1,14 @@
-__author__ = 'Mikhail Ivanov'  # type: str
-__company__ = 'IvanovsCo'
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
+__author__ = 'Mikhail Ivanov'  # type: str
+__company__ = 'IvanovsCo'
 
+# { % load staticfiles %}
 def index(request):
-    return HttpResponse("""Здесь будет информационный портал посёлка Краснокаменка.
-    Цель проекта: информационное обеспечение самоуправления и развития экономики посёлка.
-    Организатор: Михаил Иванов, +7-978-934-87-07
+    return HttpResponse("""Здесь будет информационный портал посёлка Краснокаменка.<p><p>
+    Цель проекта: информационное обеспечение самоуправления и развития экономики посёлка.<p>
+    Организатор: Михаил Иванов, +7-978-934-87-07<p>
+    <img src="static/я.jpg" alt="Михаил Иванов"/>
                         """)
+
