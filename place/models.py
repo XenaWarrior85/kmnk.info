34092ddfcac0e76594bbc1448b3ad0218a34d480
kinoafisharu <kinoafisharu@gmail.com>
2018-08-03 11:39:23 +0300

    начал проектировать модели для приложений2

---
+++ place/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Модели справочников, подлежащие замене на API надёжных первоисточников
+
+class PlaceAddress(models.Model):
+    line = models.CharField(max_length=200)
+    number = models.IntegerField(null=False)
+    dop = models.CharField(max_length=100)
+    photo = models.ImageField()
+    
