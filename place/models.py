d947dddab10cec4a64cd1252672c78150da9efef
kinoafisharu <kinoafisharu@gmail.com>
2018-08-29 16:32:29 +0300

    создал шаблон и перенес в него текст главной

--- place/models.py
+++ place/models.py
@@ -10,3 +10,9 @@ class PlaceAddress(models.Model):
     dop = models.CharField(max_length=100)
     # photo = models.ImageField()
 
+    class Meta:
+        verbose_name = 'Адрес места'
+        verbose_name_plural = 'Адреса мест'
+
+    def __str__(self):
+        return '{} {} {}'.format(self.line, self.number, self.dop)
