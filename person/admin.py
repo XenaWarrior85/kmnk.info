1ca94054d4493b5bb917688857c2773cd840aff9
evheniyonoshko <yevgenii.onoshko@djangostars.com>
2018-11-02 13:28:00 +0200

    add person model

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, Galery
+
+
+class GaleryInline(admin.TabularInline):
+    model = Galery
+    extra = 1
+    verbose_name = 'Фото'
+    verbose_name_plural = 'Фото'
+    classes = ('collapse',)
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'second_name', 'third_name', 'birthday')
+    inlines = [GaleryInline, ]
+
+
+admin.site.register(Person, PersonAdmin)
