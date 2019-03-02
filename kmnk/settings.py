59c2a4fce0c13adc513811340b8ea2eee8b12973
whytryb <offlittlehooligans@gmail.com>
2019-03-02 16:26:23 +0200

    update settings

--- kmnk/settings.py
+++ kmnk/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '151876fb-bfcb-43d8-a4e3-183addd94d7b'
+SECRET_KEY = '2b1e0efd-bf60-473b-a975-74caa7c252da'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,7 +92,7 @@ DATABASES = {'default': {}}
 try:
     DATABASES = {
         'default': dj_database_url.config(
-            default='postgres://eqchogbsbqhvqj:a407eb2a86943d2abf4e442f5653da8aeb32524ba16297e8434aeff331008ed1@ec2-54-227-246-152.compute-1.amazonaws.com:5432/d6t8l3hnfieed4'
+            default='postgres://ynbtwwzcgwjylm:0289a9c4444fdd0506dde09aee0caccc027a983ea06c57c9ab24e7fd62339af2@ec2-54-235-67-106.compute-1.amazonaws.com:5432/d9te5v66tluf3t'
         )
     }
 except UndefinedValueError:
