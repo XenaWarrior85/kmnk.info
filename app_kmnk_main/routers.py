80a211cd39e0166c3f0a1fda361cc1cc6e520bc8
Ивановы и Ко <32426965+kinoafisharu@users.noreply.github.com>
2019-03-21 09:11:24 +0300

    призыв к авторам кода - комментируйте назначение кода

--- app_kmnk_main/routers.py
+++ app_kmnk_main/routers.py
@@ -2,5 +2,5 @@ from rest_framework import routers
 from app_place.viewsets import PlaceAddressesViewSet
 
 router = routers.DefaultRouter()
-
+# если бы автором кода был я, я бы написал здесь, зачем этот роутер
 router.register(r'Place Address', PlaceAddressesViewSet)
