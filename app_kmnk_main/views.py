4056c2b0e6b29aa44805b36ea42ec0f8f6cc73c5
Ивановы и Ко <32426965+kinoafisharu@users.noreply.github.com>
2019-03-21 09:06:48 +0300

    вопрос автору кода

--- app_kmnk_main/views.py
+++ app_kmnk_main/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 __author__ = 'Mikhail Ivanov'  # type: str
 __company__ = 'Ivanov@Co'
 
-
+# каково назначение этого рендера? Можно ли и как обойтись без него?
+# не избыточен ли этот код?
 def index(request):
     return render(request, 'main.html', {})
